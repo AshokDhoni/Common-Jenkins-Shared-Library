6840c3402522884dcf3199e656d83be422217ab2
madandevops2024 <162949493+madandevops2024@users.noreply.github.com>
2024-03-29 14:10:44 +0530

    Update addition.groovy

--- vars/addition.groovy
+++ vars/addition.groovy
@@ -1,9 +1,11 @@
-def add(a, b)
-{
-   echo "${a+b}"
+def add(x,y){
+	echo "The sum of ${x} and ${y} is ${x+y}"
 }
 
-def call()
-{
-   echo "Welcome"
+def mul(x,y){
+	echo "The Mul of ${x} and ${y} is ${x*y}"
+}
+
+def call(){
+	echo "Welcome to Calculator"
 }
