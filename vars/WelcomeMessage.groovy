13fc36761aa1807d79161c9b762adfcc0c5dd7f9
dev1 <dev1@gmail.com>
2024-03-29 10:30:02 +0530

    Added first groovy file into the Shared Library

--- vars/WelcomeMessage.groovy
+++ vars/WelcomeMessage.groovy
@@ -2,3 +2,7 @@ def call() {
 	echo "Welcome to Jenkins Shared Library"
 	echo "Update the library function with new script logic"
 }
+
+def DisplayAnotherMessage(msg) {
+  echo "Message received is: ${msg}"
+}
