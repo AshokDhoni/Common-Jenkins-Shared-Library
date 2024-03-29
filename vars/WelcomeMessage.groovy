2440dc7b154cfd361a78577482bd03eb8643b8df
madandevops2024 <162949493+madandevops2024@users.noreply.github.com>
2024-03-29 13:08:29 +0530

    Update WelcomeMessage.groovy

--- vars/WelcomeMessage.groovy
+++ vars/WelcomeMessage.groovy
@@ -17,6 +17,24 @@ def call(msg1, msg2)
    mul();
 }
 
+def call(msg1, msg2, number)
+{
+    echo "${msg1} & ${msg2}"
+   mul();
+}
+
 def mul() {
     echo "from mul()"
+}
+
+def add(x,y){
+	echo "The sum of ${x} and ${y} is ${x+y}"
+}
+
+def mul(x,y){
+	echo "The Mul of ${x} and ${y} is ${x*y}"
+}
+
+def call(){
+	echo "Welcome to Calculator"
 }
